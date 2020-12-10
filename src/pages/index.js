8d94aaac53c91fb3a2357c64f72b70d8895637f5
jan-far <ligalifarouk@gmail.com>
2020-12-10 15:20:16 +0100

    Update Page Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,53 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Loading from '../Components/LoadingPage'
+import Loading from '../Components/LoadingPage';
 
 const LoadComponents = (name) => {
-  return lazy(() => import(`./Components/${name}`))
-}
+  return lazy(() => import(`./Components/${name}`));
+};
 
-const Homepage = LoadComponents('HomePage')
-const SignInPage = LoadComponents('Signin')
-const SignUpPage = LoadComponents('Signup')
+const Homepage = LoadComponents('HomePage');
+const SignInPage = LoadComponents('Signin');
+const SignUpPage = LoadComponents('Signup');
+const Dashboard = LoadComponents('Dashboard');
+const CreateParcel = LoadComponents('Create');
 
 const Pages = () => {
   return (
     <>
       <Switch>
-        <Route exact path='/'>
-          <Suspense fallback={<Loading />} >
+        <Route exact path="/">
+          <Suspense fallback={<Loading />}>
             <Homepage />
           </Suspense>
         </Route>
 
-        <Route exact path='/signin'>
-          <Suspense fallback={<Loading />} >
+        <Route exact path="/signin">
+          <Suspense fallback={<Loading />}>
             <SignInPage />
           </Suspense>
         </Route>
 
-        <Route exact path='/signup'>
-          <Suspense fallback={<Loading />} >
+        <Route exact path="/signup">
+          <Suspense fallback={<Loading />}>
             <SignUpPage />
+          </Suspense>
+        </Route>
+
+        <Route exact path="/dashboard">
+          <Suspense fallback={<Loading />}>
+            <Dashboard />
+          </Suspense>
+        </Route>
+
+        <Route exact path="/create">
+          <Suspense fallback={<Loading />}>
+            <CreateParcel />
           </Suspense>
         </Route>
       </Switch>
     </>
-  )
-}
+  );
+};
 
-export default Pages
+export default Pages;
