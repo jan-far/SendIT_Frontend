e906d1fc77ea46c2f85b1b3ad4f31a0fb3a2137e
jan-far <ligalifarouk@gmail.com>
2020-12-10 15:17:57 +0100

    Update App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
+// import LocationSearchInput from './Components/GooglePlace/indexs.js';
 // import LoadingSpinner from './Components/LoadingPage';
 import Pages from './pages';
 
 function App() {
   return (
     <Router>
+      {/* <LocationSearchInput /> */}
       <Pages />
       {/* <LoadingSpinner /> */}
     </Router>
